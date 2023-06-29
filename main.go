b4a2a40368d31b939da2888e5ea54662ac044615
Pragmatic <cryptopragmatic@protonmail.com>
2023-06-30 00:21:32 +0200

    testing

--- main.go
+++ main.go
@@ -16,13 +16,18 @@ func main() {
 	var miners []*Miner
 	db.Find(&miners)
 
+	counter := 0
+
 	for _, m := range miners {
 		if m.TelegramId != 0 {
 			var duplicate []*Miner
 			db.Where("telegram_id = ?", m.TelegramId).Find(&duplicate)
 			if len(duplicate) > 1 {
-				log.Printf("%s %d %d", m.Address, m.TelegramId, m.MinedTelegram)
+				// log.Printf("%s %d %d", m.Address, m.TelegramId, m.MinedTelegram)
+				counter++
 			}
 		}
 	}
+
+	log.Println(counter)
 }
