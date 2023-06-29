1692db87f41fad28a7e21d58bcb79f8e8259d5b4
Pragmatic <cryptopragmatic@protonmail.com>
2023-06-30 00:14:47 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	db = initDb()
+
+	var miners []*Miner
+	db.Find(&miners)
+
+	for _, m := range miners {
+		if m.TelegramId != 0 {
+			var duplicate []*Miner
+			db.Where("telegram_id = ?", m.TelegramId).Find(&duplicate)
+			if len(duplicate) > 1 {
+				log.Println(m.Address)
+			}
+		}
+	}
+}
