a475342c35c2cf7b4623b80553421e9ea5e0feb9
Pragmatic <cryptopragmatic@protonmail.com>
2023-06-30 01:20:10 +0200

    testing

--- main.go
+++ main.go
@@ -19,10 +19,45 @@ func main() {
 	counter := 0
 
 	for _, m := range miners {
-		if m.TelegramId == 0 {
-			m.TelegramId = int64(m.ID)
-			db.Save(m)
-			counter++
+		if m.TelegramId != 0 {
+			var duplicate []*Miner
+			db.Where("address = ?", m.Address).Order("mining_time").Find(&duplicate)
+			// basic := &Miner{}
+			// biggest := uint64(0)
+			if len(duplicate) > 1 {
+				for i, d := range duplicate {
+					// log.Println(i)
+					// if d.MinedTelegram > biggest {
+					// 	biggest = d.MinedTelegram
+					// }
+
+					if i < len(duplicate)-1 {
+						db.Unscoped().Delete(d)
+					} else {
+						log.Printf("%d %s %s %d", d.TelegramId, d.MiningTime, d.Address, d.MinedTelegram)
+					}
+				}
+
+				// for _, d := range duplicate {
+				// 	if d.MinedTelegram < biggest {
+				// 		d.MinedTelegram = biggest
+				// 		if err := db.Save(d).Error; err != nil {
+				// 			log.Println(err)
+				// 		}
+				// 	}
+
+				// 	log.Printf("%s %d %d", d.Address, d.TelegramId, d.MinedTelegram)
+				// }
+				// 	if !strings.HasPrefix(basic.Address, "3A") && strings.HasPrefix(d.Address, "3A") {
+				// 		basic.Address = d.Address
+				// 	}
+
+				// 	if err := db.Save(basic).Error; err != nil {
+				// 		log.Println(err)
+				// 	}
+				// }
+				counter++
+			}
 		}
 	}
 
